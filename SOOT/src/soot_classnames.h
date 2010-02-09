bd9b3a19ea77af0122412b4b596c29810e17d7c1
Steffen Mueller <smueller@cpan.org>
2010-02-09 20:24:43 +0100

    src dir

---
+++ SOOT/src/soot_classnames.h
@@ -0,0 +1,12 @@
+#ifndef __soot_classnames_h_
+#define __soot_classnames_h_
+
+namespace SOOT {
+  const unsigned int gNClassNames = 3;
+  char* gClassNames[gNClassNames] = {
+    "TObject",
+    "TH1",
+    "TH1D",
+  };
+} // end namespace SOOT
+#endif
