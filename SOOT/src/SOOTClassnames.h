0b96651d8fdf23169f3505a2856f6aa22d15eea2
Steffen Mueller <smueller@cpan.org>
2010-02-11 16:35:15 +0100

    rm autogen file

--- SOOT/src/SOOTClassnames.h
+++
@@ -1,8 +0,0 @@
-#ifndef __soot_classnames_h_
-#define __soot_classnames_h_
-
-namespace SOOT {
-  extern const unsigned int gNClassNames;
-  extern const char* gClassNames[3];
-} // end namespace SOOT
-#endif
