8b5c6b94438061257f5a38bfadb843401d611105
Steffen Mueller <smueller@cpan.org>
2010-02-11 16:33:26 +0100

    use full set of classnames

--- SOOT/buildtools/genclassnames.pl
+++ SOOT/buildtools/genclassnames.pl
@@ -2,11 +2,25 @@
 use strict;
 use warnings;
 use File::Spec;
-# TODO implement
+
 open my $h_oh, '>', File::Spec->catfile('src', 'SOOTClassnames.h') or die $!;
 open my $cc_oh, '>', File::Spec->catfile('src', 'SOOTClassnames.cc') or die $!;
 
-my $nClassNames = 3;
+my %classes;
+open my $fh, '<', File::Spec->catfile('buildtools', 'caps') or die $!;
+while (<$fh>) {
+  m{
+      ^\s*
+      //\s*
+      -*\s*
+      (?:Begin|End)?\s*
+      (\S+)
+      \s*$
+  }x or next;
+  ++$classes{$1};
+}
+close $fh;
+my $nClassNames = keys %classes;
 
 print $h_oh <<HERE;
 #ifndef __soot_classnames_h_
@@ -27,7 +41,7 @@ namespace SOOT {
   const char* gClassNames[$nClassNames] = {
 HERE
 
-print $cc_oh qq{    "$_",\n} for qw(TObject TH1 TH1D);
+print $cc_oh qq{    "$_",\n} for sort keys %classes;
 
 print $cc_oh <<'HERE';
   };
