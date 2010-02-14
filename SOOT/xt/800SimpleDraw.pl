278827d9be075e7439e4ecb248fd8715e7a5bdb3
Steffen Mueller <smueller@cpan.org>
2010-02-14 15:15:05 +0100

    simple, yet working draw example

---
+++ SOOT/xt/800SimpleDraw.pl
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+
+use SOOT;
+
+my $g = TGraph->new(3);
+$g->SetPoint(0, 1., 2.);
+$g->SetPoint(1, 3., 4.);
+$g->SetPoint(2, 5., 6.);
+my $t = TCanvas->new("myCanvas");
+$g->Draw("ALP");
+$t->SaveAs("t.eps");
+
