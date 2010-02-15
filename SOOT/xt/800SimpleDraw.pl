736a9003338b41faca850c5b40f6fbf39ec72756
Steffen Mueller <smueller@cpan.org>
2010-02-15 18:42:22 +0100

    cosmetics

--- SOOT/xt/800SimpleDraw.pl
+++ SOOT/xt/800SimpleDraw.pl
@@ -5,6 +5,7 @@ use autobox::Core;
 
 use SOOT;
 
+# FIXME, pass arrays instead (since that now works)
 my $g = TGraphErrors->new(3);
 $g->SetPoint(0, 1., 9.);
 $g->SetPointError(0, 0.5, 0.5);
@@ -14,3 +15,7 @@ $g->SetPoint(2, 5., 6.);
 $g->SetPointError(2, 0.5, 0.5);
 my $cv = TCanvas->new("myCanvas");
 $g->Draw("ALP");
+$cv->SaveAs("cv.eps");
+
+my $x = $g->GetX();
+warn Dumper $x;
