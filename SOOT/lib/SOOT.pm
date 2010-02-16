3d4107fad88491facdd8c249d16b960403109886
Steffen Mueller <smueller@cpan.org>
2010-02-16 11:56:24 +0100

    Warning about the experimental state

--- SOOT/lib/SOOT.pm
+++ SOOT/lib/SOOT.pm
@@ -1,5 +1,5 @@
 package SOOT;
-use 5.010000;
+use 5.008001;
 use strict;
 use warnings;
 
@@ -37,6 +37,20 @@ SOOT - Use ROOT from Perl
 SOOT is a Perl extension for using the ROOT library. It is very similar
 to the Ruby-ROOT or PyROOT extensions for their respective languages.
 Specifically, SOOT was implemented after the model of Ruby-ROOT.
+
+Please note that SOOT is to be considered highly experimental at this point.
+It uses a very dynamic approach to wrapping a very large and quickly
+evolving library. Due to the dynamic nature (using the CInt introspection),
+SOOT is able to handle most of the ROOT classes without explicitly
+wrapping them. Some things are expected to not work because not enough
+information about the API can be obtained automatically. Let me know
+what you need and what is giving you problems and we'll work out a solution.
+
+In order to install and use SOOT, you need a configured ROOT library.
+In particular, it is necessary that the F<root-config> tool be executable
+and findable via your C<PATH> environment variable. Alternatively, you
+may set the C<ROOTSYS> environment variable. Please refer to the ROOT
+manual for details.
 
 =head1 SEE ALSO
 
