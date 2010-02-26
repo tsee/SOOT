048ed594aa0e5bedf305cfe4c87eb880708e1744
Steffen Mueller <smueller@cpan.org>
2010-02-26 18:35:48 +0100

    disable CODE overloading for TObjects until assignment overloading works

--- SOOT/lib/TObject.pm
+++ SOOT/lib/TObject.pm
@@ -3,19 +3,19 @@ use strict;
 use warnings;
 use vars qw/$AUTOLOAD/;
 
-use overload 
-  '&{}' => sub {
-    my $obj = shift;
-    my $class = ref($obj);
-    return sub {
-      if (@_ == 1 and ref($_[0]) and ref($_[0])->isa($class)) {
-        return SOOT::CallAssignmentOperator($class, $obj, $_[0]);
-      }
-      else {
-        Carp::croak("Trying to call assignment operator without an object to copy");
-      }
-    };
-  };
+#use overload 
+#  '&{}' => sub {
+#    my $obj = shift;
+#    my $class = ref($obj);
+#    return sub {
+#      if (@_ == 1 and ref($_[0]) and ref($_[0])->isa($class)) {
+#        return SOOT::CallAssignmentOperator($class, $obj, $_[0]);
+#      }
+#      else {
+#        Carp::croak("Trying to call assignment operator without an object to copy");
+#      }
+#    };
+#  };
 
 sub AUTOLOAD {
   $AUTOLOAD =~ s/::([^:]+)$//;
