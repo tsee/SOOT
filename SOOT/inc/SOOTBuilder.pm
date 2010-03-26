69160ac5846bdd51ab92f3fcd61a3c9f7b2e8add
Steffen Mueller <smueller@cpan.org>
2010-03-26 11:07:16 +0100

    object compilation, inspired by Qt4.pm's build tools

--- SOOT/inc/SOOTBuilder.pm
+++ SOOT/inc/SOOTBuilder.pm
@@ -41,6 +41,45 @@ our @Typemaps = qw(
 #    'TYPEMAPS'    => \@typemaps,
 #    'CC'          => $CC,
 
+sub ACTION_code {
+  my $self = shift;
+  $self->SUPER::ACTION_code(@_);
+
+  $self->depends_on('build_soot');
+}
+
+sub ACTION_build_soot {
+  my $self = shift;
+  $self->depends_on('gen_constants');
+
+  #my $p = $self->{properties};
+  #local $p->{extra_compiler_flags} = [
+  #  @{$self->extra_compiler_flags},
+  #  '-Itools/puic',
+  #  '-Itools/puic/perl',
+  #];
+  my @objects;
+  my $files = $self->_find_file_by_type('cc', 'src');
+  foreach my $file (keys %$files) {
+    push(@objects, $self->compile_c($file));
+  }
+
+  #my $script_dir = File::Spec->catdir($self->blib, 'script');
+  #File::Path::mkpath( $script_dir );
+
+  #my $puic = File::Spec->catfile($script_dir, '/puic4');
+
+  #unless($self->up_to_date(\@objects, [$puic])) {
+  #  $self->_cbuilder->link_executable(
+  #    exe_file => $puic,
+  #    objects => \@objects,
+  #    extra_linker_flags => $p->{extra_linker_flags},
+  #  );
+  #}
+
+  $self->depends_on('config_data');
+}
+
 sub ACTION_gen_xsp_include {
   my $self = shift;
   system($^X, '-I.', '-Iinc', File::Spec->catfile('buildtools', 'gen_root_xsp_include.pl')) and die $!;
