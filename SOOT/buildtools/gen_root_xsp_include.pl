ecb6167e18882c74d64f93c5ef3f4905fdb93dc2
Steffen Mueller <smueller@cpan.org>
2010-03-26 11:51:19 +0100

    deal with relative paths

--- SOOT/buildtools/gen_root_xsp_include.pl
+++ SOOT/buildtools/gen_root_xsp_include.pl
@@ -4,6 +4,7 @@ use File::Spec;
 use inc::latest 'ExtUtils::Typemap';
 
 my $xsp_dir = 'ROOT_XSP';
+my $xsp_dir_compile = File::Spec->catdir(File::Spec->updir, 'ROOT_XSP');
 
 opendir my $dh, $xsp_dir or die $!;
 open my $oh_xs, '>', 'rootclasses.xsinclude' or die $!;
@@ -14,7 +15,7 @@ my $typemap = ExtUtils::Typemap->new(file => 'rootclasses.map');
 while(defined(my $file = readdir($dh))) {
   next if $file !~ /^(.+)\.xsp$/i;
   my $basename = $1;
-  my $full = File::Spec->catfile($xsp_dir, $file);
+  my $full = File::Spec->catfile($xsp_dir_compile, $file);
   print $oh_xs <<ENDXSCODE;
 
 INCLUDE_COMMAND: \$^X -MExtUtils::XSpp::Cmd -e xspp -- -t ../typemap.xsp $full
