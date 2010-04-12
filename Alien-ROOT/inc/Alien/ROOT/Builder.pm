a3198d106157fcbd6101012765328389953b12b7
Steffen Mueller <smueller@cpan.org>
2010-04-12 19:24:36 +0200

    more steps towards building root

--- Alien-ROOT/inc/Alien/ROOT/Builder.pm
+++ Alien-ROOT/inc/Alien/ROOT/Builder.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 use base 'Module::Build';
-use Alien::ROOT::Builder::Utility qw(aroot_touch);
+use Alien::ROOT::Builder::Utility qw(aroot_touch aroot_install_arch_auto_dir aroot_install_arch_auto_file);
 
 use Cwd ();
 use Carp ();
@@ -50,8 +50,6 @@ sub ACTION_code {
   # see comment in ACTION_build for why 'configured' is used
   return if -f 'configured';
   $self->depends_on( 'build_ROOT' );
-  $self->install_ROOT;
-  # see comment in ACTION_build for why 'configured' is used
   aroot_touch( 'configured' );
   $self->add_to_cleanup( 'configured' );
 }
@@ -62,81 +60,23 @@ sub ACTION_build_ROOT {
   return if not $self->notes( 'build_ROOT' );
   $self->fetch_ROOT;
   $self->extract_ROOT;
-  $self->build_root;
-}
-
-# installs to blib/
-sub ACTION_install_ROOT {
-  my $self = shift;
-  return if not $self->notes( 'build_ROOT' );
-  $self->depends_on( 'build_perl' );
-  $self->install_root;
-}
-
-
-# These are utility commands for getting into and out of our build directory
-sub _chdir_or_die {
-  use File::Spec ();
-  my $dir = File::Spec->catfile(@_);
-  chdir $dir or Carp::croak("Failed to chdir to $dir: $!");
-}
-sub _chdir_back {
-  chdir $ORIG_DIR or Carp::croak("Failed to chdir to $ORIG_DIR: $!");
-}
-
-sub ACTION_code {
-  my ($self) = @_;
-
-  my $rc = $self->SUPER::ACTION_code;
-  if ($self->notes('build_ROOT')) {
-    if ($self->notes('extra')) {
-      _chdir_or_die('libjio');
-    }
-    else {
-      _chdir_or_die('libjio', 'libjio');
-    }
-
-    # Run the make system to do the rest, but save the return code
-    system($self->notes('make'));
-    $rc = $? >> 8;
-
-    # Make sure we change the directory back before adding notes, or they
-    # won't persist (in _build state)
-    _chdir_back();
-    $self->notes(build_result => $rc);
-  }
-
-  return $rc;
+  $self->build_ROOT;
 }
 
 sub ACTION_install {
-  my ($self) = @_;
-
-  my $rc = $self->SUPER::ACTION_install;
-  if ($self->notes('build_ROOT')) {
-    # Get into our build directory
-    if ($self->notes('extra')) {
-      _chdir_or_die('libjio');
-    }
-    else {
-      _chdir_or_die('libjio', 'libjio');
-    }
-
-    # Run the make system to do the rest
-    $rc = (system($self->notes('make'), 'install') == 0) ? 1 : 0;
-    _chdir_back();
-  }
-
-  return $rc;
+  my $self = shift;
+  $self->SUPER::ACTION_code;
+  return if not $self->notes( 'build_ROOT' );
+  $self->install_ROOT;
 }
 
 sub ACTION_clean {
   my ($self) = @_;
 
   my $rc = $self->SUPER::ACTION_clean;
-  _chdir_or_die('libjio');
+  chdir($self->notes('build_data')->{directory});
   $rc = (system($self->notes('make'), 'clean') == 0) ? 1 : 0;
-  _chdir_back();
+  chdir($ORIG_DIR);
 
   return $rc;
 }
@@ -144,40 +84,66 @@ sub ACTION_clean {
 
 #ftp://root.cern.ch/root/root_v5.26.00.source.tar.gz
 sub fetch_ROOT {
-    my $self = shift;
+  my $self = shift;
 
-    return if defined $self->notes('build_data')->{archive}
-           and -f $self->notes('build_data')->{archive};
+  return if defined $self->notes('build_data')->{archive}
+         and -f $self->notes('build_data')->{archive};
 
-    $self->_load_bundled_modules;
-    print "Fetching ROOT...\n";
-    print "fetching from: ", $self->notes('build_data')->{url}, "\n";
+  $self->_load_bundled_modules;
+  print "Fetching ROOT...\n";
+  print "fetching from: ", $self->notes('build_data')->{url}, "\n";
 
-    my $ff = File::Fetch->new( uri => $self->notes('build_data')->{url} );
-    my $path = $ff->fetch(to => File::Spec->curdir);
-    die 'Unable to fetch archive' unless $path;
-    $self->notes('build_data')->{archive} = $path;
+  my $ff = File::Fetch->new( uri => $self->notes('build_data')->{url} );
+  my $path = $ff->fetch(to => File::Spec->curdir);
+  die 'Unable to fetch archive' unless $path;
+  $self->notes('build_data')->{archive} = $path;
 }
 
 sub extract_ROOT {
-    my $self = shift;
+  my $self = shift;
 
-    return if -d $self->notes( 'build_data' )->{data}{directory};
-    my $archive = $self->notes( 'build_data' )->{data}{archive};
-    if (!$archive) {
-      $self->fetch_ROOT;
-      $archive = $self->notes( 'build_data' )->{data}{archive};
-    }
+  return if -d $self->notes( 'build_data' )->{data}{directory};
+  my $archive = $self->notes( 'build_data' )->{data}{archive};
+  if (!$archive) {
+    $self->fetch_ROOT;
+    $archive = $self->notes( 'build_data' )->{data}{archive};
+  }
 
-    print "Extracting wxWidgets...\n";
+  print "Extracting wxWidgets...\n";
 
-    $self->_load_bundled_modules;
-    $Archive::Extract::PREFER_BIN = 1;
-    my $ae = Archive::Extract->new( archive => $archive );
+  $self->_load_bundled_modules;
+  $Archive::Extract::PREFER_BIN = 1;
+  my $ae = Archive::Extract->new( archive => $archive );
 
-    die 'Error: ', $ae->error unless $ae->extract;
+  die 'Error: ', $ae->error unless $ae->extract;
 
-    #$self->patch_ROOT;
+  #$self->patch_ROOT;
 }
+
+sub build_ROOT {
+  my $self = shift;
+
+  my $prefix = $self->aroot_install_arch_auto_dir('root');
+  my @cmd = (
+    qw(sh configure),
+    '--prefix', $prefix,
+    '--etcdir', File::Spec->catfile($prefix, 'etc'),
+    '--enable-explicitlink', # needed for SOOT
+  );
+
+  my $dir = $self->notes('build_data')->{directory};
+  chdir $dir;
+
+  # do not reconfigure unless necessary
+  # print $cmd, "\n";
+  if (not -f 'Makefile') {
+    system(@cmd) and die "Build failed while running '@cmd': $?";
+  }
+  my $make = $self->notes('build_data')->{make};
+  system($make) and die "Build failed while running '$make': $?";
+  chdir $ORIG_DIR;
+}
+
+
 
 1;
--- Alien-ROOT/inc/Alien/ROOT/Builder/Utility.pm
+++ Alien-ROOT/inc/Alien/ROOT/Builder/Utility.pm
@@ -7,35 +7,9 @@ use base qw(Exporter);
 use Config;
 use Fatal qw(open);
 
-our @EXPORT_OK = qw(aroot_arch_file aroot_install_arch_file
-                    aroot_install_arch_auto_file
-                    aroot_arch_dir aroot_install_arch_dir aroot_touch);
-
-sub aroot_arch_file {
-  my( $vol, $dir, $file ) = File::Spec->splitpath( $_[0] || '' );
-  File::Spec->catfile( 'blib', 'arch', 'Alien', 'ROOT',
-                       File::Spec->splitdir( $dir ), $file );
-}
-
-sub aroot_arch_dir {
-  my( $vol, $dir, $file ) = File::Spec->splitpath( $_[0] || '' );
-  File::Spec->catdir( 'blib', 'arch', 'Alien', 'ROOT',
-                      File::Spec->splitdir( $dir ), $file );
-}
-
-sub aroot_install_arch_file {
-  my( $build, $p ) = @_;
-  my( $vol, $dir, $file ) = File::Spec->splitpath( $p || '' );
-  File::Spec->catfile( $build->install_destination( 'arch' ), 'Alien', 'ROOT',
-                       File::Spec->splitdir( $dir ), $file );
-}
-
-sub aroot_install_arch_dir {
-  my( $build, $p ) = @_;
-  my( $vol, $dir, $file ) = File::Spec->splitpath( $p || '' );
-  File::Spec->catdir( $build->install_destination( 'arch' ), 'Alien', 'ROOT',
-                      File::Spec->splitdir( $dir ), $file );
-}
+our @EXPORT_OK = qw(aroot_install_arch_auto_file
+                    aroot_install_arch_auto_dir
+                    aroot_touch);
 
 sub aroot_install_arch_auto_file {
   my( $build, $p ) = @_;
@@ -44,10 +18,17 @@ sub aroot_install_arch_auto_file {
                        File::Spec->splitdir( $dir ), $file );
 }
 
+sub aroot_install_arch_auto_dir {
+  my( $build, $p ) = @_;
+  my( $vol, $dir, $file ) = File::Spec->splitpath( $p || '' );
+  File::Spec->catdir( $build->install_destination( 'arch' ), 'auto', 'Alien', 'ROOT',
+                      File::Spec->splitdir( $dir ), $file );
+}
+
 sub aroot_touch {
-    require ExtUtils::Command;
-    local @ARGV = @_;
-    ExtUtils::Command::touch();
+  require ExtUtils::Command;
+  local @ARGV = @_;
+  ExtUtils::Command::touch();
 }
 
 1;
