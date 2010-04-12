531b44b5264d4a29240d04ee461cd5eeccfe5442
Steffen Mueller <smueller@cpan.org>
2010-04-12 19:30:24 +0200

    add bundling script

---
+++ Alien-ROOT/buildtools/bundle.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use inc::latest;
+
+#               Module::Build
+#               version
+foreach my $module (qw(
+               Locale::Maketext::Simple
+               Params::Check
+               Module::Load
+               Module::Load::Conditional
+               IPC::Cmd
+               Archive::Extract
+               File::Fetch
+               Capture::Tiny))
+{
+  inc::latest->bundle_module($module, 'inc');
+}
+
