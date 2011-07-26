c596f833b48c3652a3eda8b83197c9dc11a93499
Steffen Mueller <smueller@cpan.org>
2011-07-26 17:12:32 +0200

    Lazier and more robust building

--- SOOT/inc/SOOTBuilder.pm
+++ SOOT/inc/SOOTBuilder.pm
@@ -24,30 +24,6 @@ BEGIN {
 }
 
 
-# utilities...
-##############
-sub striprun {
-  my $inc = `@_`;
-  chomp $inc;
-  return $inc;
-}
-
-# check if we can run some command (From Module::Install::Can)
-sub can_run {
-  my ($cmd) = @_;
-
-  my $_cmd = $cmd;
-  return $_cmd if (-x $_cmd or $_cmd = MM->maybe_command($_cmd));
-
-  for my $dir ((split /$Config::Config{path_sep}/, $ENV{PATH}), '.') {
-    next if $dir eq '';
-    my $abs = File::Spec->catfile($dir, $_[1]);
-    return $abs if (-x $abs or $abs = MM->maybe_command($abs));
-  }
-
-  return;
-}
-
 ##################
 
 sub ACTION_code {
@@ -57,8 +33,16 @@ sub ACTION_code {
   $self->SUPER::ACTION_code(@_);
 }
 
-sub _build_dictionaries {
+sub ACTION_build_dictionaries {
   my $self = shift;
+
+  my $outfile = File::Spec->catfile('src', 'SOOTDictionary.cc');
+  my @infiles = (
+    File::Spec->catfile('src', 'TExecImpl.h'),
+    File::Spec->catfile('src', 'LinkDef.h'),
+  );
+  my $latest = _latest_file_mod_time(@infiles);
+  return if -f $outfile and -M $outfile < $latest;
 
   my $bindir = $Alien->bindir;
   my $rootcint = File::Spec->catfile($bindir, 'rootcint');
@@ -73,10 +57,9 @@ sub _build_dictionaries {
   my @cmd = (
     $rootcint,
     '-f',
-    File::Spec->catfile('src', 'SOOTDictionary.cc'),
+    $outfile,
     '-c',
-    File::Spec->catfile('src', 'TExecImpl.h'),
-    File::Spec->catfile('src', 'LinkDef.h'),
+    @infiles
   );
   system(@cmd)
     and die "Failed to run '@cmd'. Exit value: " . ($?>>8);
@@ -86,6 +69,7 @@ sub ACTION_build_soot {
   my $self = shift;
   $self->depends_on('gen_constants');
   $self->depends_on('gen_xsp_include');
+  $self->depends_on('build_dictionaries');
   $self->depends_on('merge_typemaps');
 
   #my $p = $self->{properties};
@@ -95,7 +79,6 @@ sub ACTION_build_soot {
   #  '-Itools/puic/perl',
   #];
 
-  $self->_build_dictionaries;
 
   my @objects;
   my $files = $self->_find_file_by_type('cc', 'src');
@@ -122,11 +105,52 @@ sub ACTION_build_soot {
 
 sub ACTION_gen_xsp_include {
   my $self = shift;
-  system($^X, '-I.', '-Iinc', File::Spec->catfile('buildtools', 'gen_root_xsp_include.pl')) and die $!;
+  #system($^X, '-I.', '-Iinc', File::Spec->catfile('buildtools', 'gen_root_xsp_include.pl')) and die $!;
+  my $xsp_dir = 'ROOT_XSP';
+  my $xsp_dir_compile = File::Spec->catdir(File::Spec->updir, 'ROOT_XSP');
+
+  my @outfiles = (
+    'rootclasses.xsinclude',
+    'rootclasses.h',
+    'rootclasses.map',
+  );
+
+  opendir my $dh, $xsp_dir or die $!;
+  my @infiles = grep -f File::Spec->catfile($xsp_dir, $_), grep /\.xsp$/i, readdir($dh);
+
+  my $latest_in    = _latest_file_mod_time(map File::Spec->catfile($xsp_dir, $_), @infiles);
+  my $earliest_out = _earliest_file_mod_time(@outfiles);
+
+  return 1 if defined $earliest_out and defined $latest_in and $earliest_out < $latest_in;
+
+  print "Regenerating the XS++ include files...\n";
+  open my $oh_xs, '>', 'rootclasses.xsinclude' or die $!;
+  open my $oh_h, '>', 'rootclasses.h' or die $!;
+  unlink('rootclasses.map');
+  my $typemap = ExtUtils::Typemap->new(file => 'rootclasses.map');
+
+  while(defined(my $file = shift @infiles)) {
+    next if $file !~ /^(.+)\.xsp$/i;
+    my $basename = $1;
+    my $full = File::Spec->catfile($xsp_dir_compile, $file);
+    print $oh_xs <<ENDXSCODE;
+
+INCLUDE_COMMAND: \$^X -MExtUtils::XSpp::Cmd -e xspp -- -t ../typemap.xsp $full
+
+ENDXSCODE
+    print $oh_h <<ENDHCODE;
+#include <$basename.h>
+ENDHCODE
+    
+    $typemap->add_typemap(ctype => "$basename *", xstype => 'O_OBJECT');
+  }
+
+  $typemap->write();
 }
 
 sub ACTION_gen_constants {
   my $self = shift;
+  print "Regenerating constants...\n";
   system($^X, '-I.', '-Iinc', File::Spec->catfile('buildtools', 'genconstants.pl')) and die $!;
 }
 
@@ -157,17 +181,15 @@ sub ACTION_merge_typemaps {
 
   my $typemap = 'typemap';
   if (-f $typemap) { # lazy!
-    my $latest = 1e9;
-    foreach my $file (@Typemaps) {
-      $latest = -s _ if -s $file < $latest;
-    }
-    if (-s $typemap > $latest) {
+    my $latest_file = _latest_file(@Typemaps, $typemap);
+    if ($latest_file eq $typemap) {
       return 1;
     }
     unlink $typemap;
   }
 
   print "Merging custom typemaps...\n";
+  sleep 1;
   my $outmap = ExtUtils::Typemap->new(file => 'typemap');
   foreach my $typemap_file (@Typemaps) {
     print "... merging $typemap_file\n";
@@ -178,3 +200,60 @@ sub ACTION_merge_typemaps {
   return 1;
 }
 
+# utilities...
+##############
+sub striprun {
+  my $inc = `@_`;
+  chomp $inc;
+  return $inc;
+}
+
+# check if we can run some command (From Module::Install::Can)
+sub can_run {
+  my ($cmd) = @_;
+
+  my $_cmd = $cmd;
+  return $_cmd if (-x $_cmd or $_cmd = MM->maybe_command($_cmd));
+
+  for my $dir ((split /$Config::Config{path_sep}/, $ENV{PATH}), '.') {
+    next if $dir eq '';
+    my $abs = File::Spec->catfile($dir, $_[1]);
+    return $abs if (-x $abs or $abs = MM->maybe_command($abs));
+  }
+
+  return;
+}
+
+# Return the name of the newest file in the input list
+sub _latest_file {
+  my $latest;
+  my $latest_file;
+  foreach my $file (@_) {
+    my $m = -M $file;
+    next if not defined $m;
+    $latest = $m, $latest_file = $file if not defined $latest or $m < $latest;
+  }
+  return $latest_file;
+}
+
+# Return the -M time of the newest file in the input list
+sub _latest_file_mod_time {
+  my $latest;
+  foreach my $file (@_) {
+    my $m = -M $file;
+    next if not defined $m;
+    $latest = $m if not defined $latest or $m < $latest;
+  }
+  return $latest;
+}
+
+# Return the -M time of the oldest file in the input list
+sub _earliest_file_mod_time {
+  my $earliest;
+  foreach my $file (@_) {
+    my $m = -M $file;
+    next if not defined $m;
+    $earliest = $m if not defined $earliest or $m > $earliest;
+  }
+  return $earliest;
+}
