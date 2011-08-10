d170f75a07e72ade34136fcbbccac91fed982cbd
Steffen Mueller <smueller@cpan.org>
2011-08-10 17:17:06 +0200

    global variable completion

--- SOOT-App/lib/Devel/REPL/Plugin/CompletionDriver/SOOT.pm
+++ SOOT-App/lib/Devel/REPL/Plugin/CompletionDriver/SOOT.pm
@@ -10,7 +10,6 @@ sub BEFORE_PLUGIN {
 }
 
 # returns array ref: [$ok, $show_prototype, $methname, $invocant]
-
 sub _match_method_call {
   my ($self, $last) = @_;
   my $show_prototype = 0;
@@ -39,18 +38,16 @@ sub _match_method_call {
   return [1, $show_prototype, $last, $invocant];
 }
 
-sub _try_complete_lexical {
-  my ($self, $invocant, $methname, $document, $show_prototype) = @_;
-  
-  my $soot_obj;
-  my $invocant_str = $invocant->content;
-  my $lexenv = $self->lexical_environment();
-  my $cxt = $lexenv->get_context('_');
-  my $var = $cxt->{$invocant_str};
-
-  return
+sub _is_soot_object {
+  my $var = shift;
+  return()
     if not blessed($var)
     or not ($var->isa('TObject') or SOOT::API->is_soot_class(ref($var)));
+  return 1;
+}
+
+sub _show_matching_methods {
+  my ($self, $var, $invocant, $methname, $document, $show_prototype) = @_;
 
   my $class = $var->Class;
   if ($show_prototype) {
@@ -71,6 +68,57 @@ sub _try_complete_lexical {
   }
 }
 
+sub _try_complete_lexical {
+  my ($self, $invocant, $methname, $document, $show_prototype) = @_;
+  
+  my $invocant_str = $invocant->content;
+  my $lexenv = $self->lexical_environment();
+  my $cxt = $lexenv->get_context('_');
+  my $var = $cxt->{$invocant_str};
+
+  return if not _is_soot_object($var);
+
+  return $self->_show_matching_methods($var, $invocant, $methname, $document, $show_prototype);
+}
+
+sub _try_complete_global {
+  my ($self, $invocant, $methname, $document, $show_prototype) = @_;
+  
+  my $invocant_str = $invocant->content;
+
+  my $sigil = $invocant_str =~ s/^([\$\@\%\&\*])// ? $1 : 0; # remove sigil
+
+  my @package_fragments = split qr/::|'/, $invocant_str;
+
+  # Almost verbatim from the ::Globals completion driver...
+
+  # split drops the last fragment if it's empty
+  push @package_fragments, '' if $invocant_str =~ /(?:'|::)$/;
+
+  # the beginning of the variable, or an incomplete package name
+  my $incomplete = pop @package_fragments;
+
+  # recurse for the complete package fragments
+  my $stash = \%::;
+  for (@package_fragments) {
+    $stash = $stash->{"$_\::"};
+  }
+
+  # collect any variables from this stash
+  my @found = grep { $_ eq $invocant_str }
+              keys %$stash;
+  return if not @found;
+
+  my $varname = shift(@found);
+  my $var = $stash->{$varname};
+  $var = *{$var}{SCALAR};
+  $var = $$var if $var;
+
+  return if not _is_soot_object($var);
+
+  return $self->_show_matching_methods($var, $invocant, $methname, $document, $show_prototype);
+}
+
 around complete => sub {
   my $orig = shift;
   my ($self, $text, $document) = @_;
@@ -82,7 +130,12 @@ around complete => sub {
   my ($ok, $show_prototype, $methname, $invocant) = @{$rv||[]};
   return if not $ok;
 
-  my $completions = $self->_try_complete_lexical($invocant, $methname, $document, $show_prototype);
+  my $completions;
+
+  $completions = $self->_try_complete_lexical($invocant, $methname, $document, $show_prototype);
+  return $orig->(@_), @$completions if $completions;
+
+  $completions = $self->_try_complete_global($invocant, $methname, $document, $show_prototype);
   return $orig->(@_), @$completions if $completions;
 
   return $orig->(@_);
@@ -95,6 +148,11 @@ __END__
 =head1 NAME
 
 Devel::REPL::Plugin::CompletionDriver::SOOT - Complete SOOT method names
+
+=head1 ACKNOWLEDGMENTS
+
+Contains some code adapted from L<Devel::REPL::Plugin::CompletionDriver::LexEnv>
+and L<Devel::REPL::Plugin::CompletionDriver::Globals>.
 
 =head1 AUTHOR
 
