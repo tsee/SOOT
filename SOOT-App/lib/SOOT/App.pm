ef4723bcbdd2fd60d16a172631167431659affa6
Steffen Mueller <smueller@cpan.org>
2011-08-10 17:17:25 +0200

    changelog, version bump, copyright update

--- SOOT-App/lib/SOOT/App.pm
+++ SOOT-App/lib/SOOT/App.pm
@@ -9,7 +9,7 @@ use Capture::Tiny qw/capture/;
 use Time::HiRes 'usleep';
 use vars '%SIG';
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 our $AppThread;
 
 sub usage {
@@ -150,7 +150,7 @@ Steffen Mueller, E<lt>smueller@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2010 by Steffen Mueller
+Copyright (C) 2010, 2011 by Steffen Mueller
 
 SOOT, the Perl-ROOT wrapper, is free software; you can redistribute it and/or modify
 it under the same terms as ROOT itself, that is, the GNU Lesser General Public License.
